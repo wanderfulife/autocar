44c3947bea47a5dc5c751818bce8d7412e498644
wanderfulife <john.wander@hotmail.fr>
2024-04-12 10:26:43 +0200

    starter

---
+++ src/components/reponses.js
@@ -0,0 +1,16 @@
+const nbPersArret = [
+	{ id: 1, output: "<5", text: "< 5" },
+	{ id: 2, output: "5-15", text: "5 – 15" },
+	{ id: 3, output: ">15", text: "> 15" },
+];
+
+const OptionsTransport = [
+	{ id: 1, output: "FlixBus", text: "FlixBus" },
+	{ id: 2, output: "BlablacarOuibus", text: "Blablacar / Ouibus" },
+	{ id: 3, output: "Flibco", text: "Flibco (pour les aéroports)" },
+	{ id: 4, output: "Autre", text: "Autre (préciser) :" },
+];
+
+
+
+export { nbPersArret, OptionsTransport };
